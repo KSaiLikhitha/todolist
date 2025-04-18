5ebff14931d3123abe4ed3827b84d380d7c1fe00
KSaiLikhitha <kambojisailikhitha@gmail.com>
2025-04-18 13:09:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ app.use(express.static(path.join(__dirname, 'frontend')));
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
-const DB_FILE = path.join(__dirname, 'todos.db');
+const DB_FILE = process.env.DB_PATH || path.join(__dirname, 'todos.db');
+
 
 // Ensure the todos.db file exists (important for OpenShift PVC)
 if (!fs.existsSync(DB_FILE)) {
